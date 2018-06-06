ce7119ff8ba68c14492109e609a39f12a43f62e7
Tim Chen <b05601005@ntu.edu.tw>
2018-06-06 20:44:05 +0800

    add filter function

--- shiny_app/split/server.R
+++ shiny_app/split/server.R
@@ -21,39 +21,22 @@ function(input, output, session) {
                                                             build_state == input$select.function,
                                                             house_age %in% input$slide.age[1]:input$slide.age[2],
                                                             total_size >= input$slide.area.size[1] & total_size <= input$slide.area.size[2],
-                                                            PRICE >= input$slide.low.price & PRICE <= input$slide.high.price)
+                                                            PRICE >= input$slide.low.price & PRICE <= input$slide.high.price) %>% 
+                                   select(lat,lng)
   })
   
-  output$summary <- renderPrint({
-    data = select.data()
-    nrow(data)
-  })
+ 
   
-  #b <- input$select.usage 
-  #c <- input$radio.land
-  #d <- input$radio.building
-  #e <- input$radio.car.park
-  #f <- input$select.function
-  #g <- input$slide.age
-  #h <- input$slide.area.size
-  #i <- input$slide.low.price
-  #j <- input$slide.high.price
-  #k <- input$checkGroup.facility
-  #l<- input$checkGroup.facility
-
-  
-
-  
-  points <- eventReactive(input$recalc, {
-    cbind(rnorm(10) * 12 + 5, rnorm(10) * 2+ 5)
-  }, ignoreNULL = FALSE)
+  # points <- eventReactive(input$recalc, {
+  #   cbind(rnorm(10) * 12 + 5, rnorm(10) * 2+ 5)
+  # }, ignoreNULL = FALSE)
   
   output$mymap <- renderLeaflet({
     leaflet() %>%
       addProviderTiles(providers$Stamen.TonerLite,
                        options = providerTileOptions(noWrap = TRUE)
       ) %>%
-      addMarkers(data = points())
+      addMarkers(data = select.data())
   })
   
 }
